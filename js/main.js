8f0c42e758d57a6bd41040e2156dbcd3d124da7d
Takeru Kayamori <takeru.kayamori@gmail.com>
2021-09-09 00:09:02 +0800

    All done

--- js/main.js
+++ js/main.js
@@ -45,15 +45,24 @@ const toTopBtn = document.querySelector('.to-top')
 
 function showBtn() {
   let pageY = window.pageYOffset
-  if(pageY > 800) {
-    header.classList.add('open')
-  } else if(pageY > 400){
+  console.log(pageY)
+  if(pageY > 160){
     toTopBtn.classList.add('open')
-    header.classList.remove('open')
+    header.classList.add('open')
   } else {
+    header.classList.remove('open')
     toTopBtn.classList.remove('open')
 
   }
+  // if(pageY > 800) {
+  //   header.classList.add('open')
+  // } else if(pageY > 400){
+  //   toTopBtn.classList.add('open')
+  //   header.classList.remove('open')
+  // } else {
+  //   toTopBtn.classList.remove('open')
+
+  // }
 }
 
 toTopBtn.addEventListener('click', () => window.scrollTo(0, 0))
@@ -62,7 +71,8 @@ toTopBtn.addEventListener('click', () => window.scrollTo(0, 0))
 // articleページのみで行うので、URLを確認
 if(location.href.includes('article')) {
   let titleContent = document.querySelector('.breadcrumb__article-title').textContent
-  if(titleContent.length > 10) {
+  if(titleContent.length > 10 && window.innerWidth < 380) {
+    console.log(window.innerWidth)
     const shortTitle = titleContent.slice(0, 12)
     console.log(shortTitle)
     document.querySelector('.breadcrumb__article-title').textContent = shortTitle + '...';
